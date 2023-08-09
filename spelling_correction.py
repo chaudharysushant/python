b52e990a924f42fcaba6b8a3c56b03511f7988ee
Sushant Chaudhary <72001333+chaudharysushant@users.noreply.github.com>
2023-08-09 10:17:28 +0545

    Add files via upload

---
+++ spelling_correction.py
@@ -0,0 +1,16 @@
+from textblob import TextBlob
+def convert(string):
+    li = list(string.split())
+    return li
+str1 = input("Enter your word.")
+words = convert(str1)
+corrected_words = []
+
+for i in words:
+    corrected_words.append(TextBlob(i))
+
+print("Incorrect words:", words)
+print("Corrected words:")
+
+for i in corrected_words:
+    print(i.correct(),end="")
